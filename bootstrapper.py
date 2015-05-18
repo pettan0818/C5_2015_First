f16f9a6cc6a3d00e3608b3d510ee6d3bea2894c2
pettan0818 <pettan0818+git@gmail.com>
2015-05-19 01:01:30 +0900

    [Arrangemnt] Obey pep8.

--- bootstrapper.py
+++ bootstrapper.py
@@ -14,21 +14,31 @@
 #
 """
 
-import sys
+# import sys
 import mecab_direct_connecter
+import dic_searcher
 
 
 if __name__ == '__main__':
     TARGET_FILE_NAME = "test.list"
-#     TARGET_FILE_NAME = sys.argv[1]
+    # TARGET_FILE_NAME = sys.argv[1]
 
     TEXT_LIST = [text.rstrip('\n') for text in file(TARGET_FILE_NAME, 'r')]
 
-    mecab_exe = mecab_direct_connecter.MecabMother()
+    MECAB_EXE = mecab_direct_connecter.MecabMother()
+    WORD_OPINION_TELLER = dic_searcher.OpinionDictSearcher("./opinion_dict_shrinked.dic")
 
-    mecab_exe.set_text_to_parse(TEXT_LIST[0])
+    FULL_EXP = []
 
-    mecab_exe.unknown_word_buster_by_parts()
+    for i in TEXT_LIST:
+        MECAB_EXE.set_text_to_parse(i)
 
-    TARGET_WORDS = mecab_exe.extract_category_originalshape(["名詞", "動詞"])
+        MECAB_EXE.unknown_word_buster_by_parts()
 
+        TARGET_WORDS = MECAB_EXE.extract_category_originalshape(["名詞", "動詞"])
+
+        opt_list = [WORD_OPINION_TELLER.tell_word_score(word) for word in TARGET_WORDS]
+
+        FULL_EXP.append(sum(opt_list))
+
+    print "Full Exp: %s" % str(sum(FULL_EXP))
