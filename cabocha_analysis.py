97cff7c82223de348416672925a5a23085630eab
pettan0818 <pettan0818+git@gmail.com>
2015-06-03 17:01:17 +0900

    [New Func] Cabocha Plugin testing...

---
+++ cabocha_analysis.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+
+"""
+#
+# Author:   Noname
+# URL:      https://github.com/pettan0818
+# License:  MIT License
+# Created:  2015-06-03
+#
+
+# Usage
+#
+"""
+
+import CaboCha
+
+cabocha_parser = CaboCha.parser("-d /usr/local/Cellar/mecab/0.996/lib/mecab/dic/mecab-ipadic-neologd")
