a370a295c580ce42d74dc639a7d2e0b9aba1d3b9
pettan0818 <pettan0818+git@gmail.com>
2015-06-04 10:21:10 +0900

    [cabocha impl] we can now call cabocha from python scripts.

--- cabocha_analysis.py
+++ cabocha_analysis.py
@@ -15,5 +15,12 @@
 """
 
 import CaboCha
+from bs4 import BeautifulSoup
 
-cabocha_parser = CaboCha.parser("-d /usr/local/Cellar/mecab/0.996/lib/mecab/dic/mecab-ipadic-neologd")
+cabocha_parser = CaboCha.Parser("-d /usr/local/Cellar/mecab/0.996/lib/mecab/dic/mecab-ipadic-neologd")
+
+tree = cabocha_parser.parse("「疲れる」と感じさせるような友人は、有益なつきあいとは言えないのですから、関係を絶っても問題はありません。")
+
+xml_tree = BeautifulSoup(tree.toString(CaboCha.CABOCHA_FORMAT_XML), "xml")
+
+print xml_tree.findAll("chunk")[1]
