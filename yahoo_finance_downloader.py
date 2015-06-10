2a9f185370d0a8457cd9e95006f125fd705ef662
pettan0818 <pettan0818+git@gmail.com>
2015-06-10 22:44:41 +0900

    [New] Prototype of curation system is completed.

--- yahoo_finance_downloader.py
+++ yahoo_finance_downloader.py
@@ -15,39 +15,87 @@
 """
 
 import sys
+import time
+import cPickle
 from bs4 import BeautifulSoup
 import urllib2
 
-def data_downloader(url):
-    """
-    """
-    pass
 
-def url_generator_thread(seed_url):
+def data_downloader(target_url):
     """
+    BeautifulSoupでパースしたHTMLデータを返す。
     """
-    pass
+    return BeautifulSoup(urllib2.urlopen(target_url))
+
+
+def url_generator_thread(seed_num):
+    """
+    >>> url_generator_thread("3318")
+    'http://textream.yahoo.co.jp/message/1003318/3318'
+    """
+    return "http://textream.yahoo.co.jp/message/100" + seed_num + "/" + seed_num
+
 
 def previous_url_parser(site_data):
     """
+    より前の発言のURLを取得
     """
-    pass
+    prev_url = site_data.findAll("li", class_="prev")[0].a.get("href")
 
-def comment_parser():
+    return prev_url
+
+
+def comment_parser(site_data):
     """
+    ページ内の発言をパースしてリストにして返す。
     """
-    pass
+    comment_list = [x.text.strip().replace('\n', '').replace('\r', '') for x in site_data.findAll("p", class_="comText")]
+
+    return comment_list
+
+
+def output_file_maker(result_list, output_file_name):
+    """
+    出力用コード
+    """
+    output_con = file(output_file_name, 'w')
+
+    [output_con.writelines([line + '\n' for line in x]) for x in result_list]
 
 
 if __name__ == '__main__':
-    URL = sys.argv[1]
+    import doctest
+    doctest.testmod()
 
-    SITE_DATA = BeautifulSoup(urllib2.urlopen(URL))
+    COMMENT_RESULT = []
 
-    COMMENT_LIST = [x.text.strip().replace('\n', '') for x in SITE_DATA.findAll("p", class_="comText")]
+    STOCK_NUM = sys.argv[1]
+    OUTPUT_FILE_NAME = sys.argv[2]
 
-    for x in COMMENT_LIST:
-        print x
+    SEED_URL = url_generator_thread(STOCK_NUM)
 
-    print SITE_DATA.findAll("li", class_="prev")[0].a.get("href")
+    FIRST_DATA = data_downloader(SEED_URL)
 
+    COMMENT_RESULT.append(comment_parser(FIRST_DATA))
+
+    NEXT_TARGET_URL = previous_url_parser(FIRST_DATA)
+
+    while True:
+        SITE_DATA_IN_LOOP = data_downloader(NEXT_TARGET_URL)
+
+        COMMENT_RESULT.append(comment_parser(SITE_DATA_IN_LOOP))
+
+        try:
+            NEXT_TARGET_URL = previous_url_parser(SITE_DATA_IN_LOOP)
+        except AttributeError:
+            break
+
+        print COMMENT_RESULT
+
+        print NEXT_TARGET_URL
+
+        time.sleep(5)
+
+    cPickle.dump(COMMENT_RESULT, file("test.dump", 'w'))
+
+    output_file_maker(COMMENT_RESULT, OUTPUT_FILE_NAME)
