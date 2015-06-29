b7ce66d83be07da76e43607101f1beb8d05d9dc9
pettan0818 <pettan0818+git@gmail.com>
2015-06-30 01:46:04 +0900

    [Clean] Parser become more readable(test is now included in debug func         and early definition is abolished.) [add] Now this script can find thread num.

--- yahoo_finance_downloader.py
+++ yahoo_finance_downloader.py
@@ -52,6 +52,17 @@ def previous_url_parser(site_data):
     return prev_url
 
 
+def find_my_thread_pos(site_data):
+    """
+    現在のスレッド番号を検出する
+    """
+    thread_num = site_data.findAll("li", class_="threadBefore")[0].a.get("href")
+
+    thread_num = int(thread_num.split("/")[-1])
+
+    return thread_num + 1
+
+
 def comment_parser(site_data):
     """
     ページ内の発言をパースしてリストにして返す。
@@ -70,18 +81,28 @@ def comment_parser(site_data):
     return pandas.DataFrame(data=binding_data, index=None, columns=["comments", "time", "positive", "negative"])
 
 
-def main(stock_num, output_file_name):
+def main(args):
     """
     argparser対応を進めるのでメイン関数処理は、こちらに移行させる。
     """
+    # 引数の処理
+    stock_num = args.stock_num
+    output_file_name = args.output_file_name
+
+    # 結果の格納用のデータフレームを初期化
     comment_dataframe = pandas.DataFrame(columns=["comments", "time", "positive", "negative"])
 
+    # 株式番号を元に初回処理対象URLを決定
     seed_url = url_generator_without_thread(stock_num)
 
+    # 初回処理対象のページデータをダウンロード
+    print "next page is %s" % seed_url
     first_data = data_downloader(seed_url)
 
+    # 初回データを結果格納用のデータフレームに格納
     comment_dataframe = pandas.concat([comment_dataframe, comment_parser(first_data)])
 
+    # 次の発言を取りに行くために、次のページのリンクを抽出
     next_target_url = previous_url_parser(first_data)
 
     while True:
@@ -99,17 +120,7 @@ def main(stock_num, output_file_name):
         time.sleep(10)
 
     cPickle.dump(comment_dataframe, file("test.dump", 'w'))
-
     comment_dataframe.to_csv(output_file_name, index=None)
-
-
-def test():
-    """
-    Doctestを走らせるための関数
-    ArgParseで--debugオプション時に発動。
-    """
-    import doctest
-    doctest.testmod()
 
 
 def debug(args):
@@ -117,26 +128,22 @@ def debug(args):
     Loggingを有効化する。
     """
     import logging
+    import doctest
+
+    doctest.testmod()
 
     logging.basicConfig(filename="debug.log", level=logging.DEBUG)
 
-    main(args.stock_num, args.output_file_name)
+    main(args)
 
 
 if __name__ == '__main__':
     import argparse
 
     PARSER = argparse.ArgumentParser(description="このアプリケーションは、テキストリームから指定の株式番号のスレッド書き込みをダウンロードします。", epilog="Made by pettan0818")
-    PARSER.add_argument("-t", "--test", action="store_true", help="process doctest")
-
-    TEST_OR_NOT = PARSER.parse_known_args()
-
-    if TEST_OR_NOT[0].test is True:
-        test()
-
-    PARSER.add_argument("stock_num")
+    PARSER.add_argument("stock_num", type=int)
     PARSER.add_argument("output_file_name")
-
+    PARSER.add_argument("thread_length", default=1)
     PARSER.add_argument("-d", "--debug", action="store_true", help="enter debugmode")
 
     ARGS = PARSER.parse_args()
@@ -144,4 +151,4 @@ if __name__ == '__main__':
     if ARGS.debug is True:
         debug(ARGS)
     else:
-        main(ARGS.stock_num, ARGS.output_file_name)
+        main(ARGS)
